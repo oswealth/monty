bd7753b20dd225645969e992239fe68fca249124
sixth_son6 <sudais2408@gmail.com>
2023-11-16 14:22:14 -0800

    pall.c added

---
+++ pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * m_pall - prints all the values on the stack
+ * @stack: pointer to the stack
+ * @number: line number of the opcode
+ */
+
+void m_pall(stack_t **stack, unsigned int number)
+{
+	stack_t *h;
+	(void) number;
+
+	h = *stack;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
