fa902eaa56fa8b5b6ace027fbb6dababfee37578
sixth_son6 <sudais2408@gmail.com>
2023-11-16 14:24:53 -0800

    nop.c added

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * m_nop - does nothing
+ * @stack: pointer to the stack
+ * @number:  number of the opcode
+ */
+
+void m_nop(stack_t **stack, unsigned int number)
+{
+	(void) stack;
+	(void) number;
+}
